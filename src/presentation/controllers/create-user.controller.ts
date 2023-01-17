82efce74b652c0cffafa72630659991db9e954dc
Lucas Gonçalves <lucas.floripa@icloud.com>
2023-01-17 20:49:19 -0300

    feat: ensure createUserController calls Validation with correct params

--- src/presentation/controllers/create-user.controller.ts
+++ src/presentation/controllers/create-user.controller.ts
@@ -1,12 +1,15 @@
 import { CreateUserImplementation } from 'domain/implementation'
-import { Controller, HttpResponse } from '../protocols'
+import { Controller, HttpResponse, Validation } from '../protocols'
 
 export class CreateUserController implements Controller {
   constructor (
-    private readonly createUserImplementation: CreateUserImplementation
+    private readonly createUserImplementation: CreateUserImplementation,
+    private readonly validation: Validation
   ) {}
 
   async handle (request: CreateUserController.Params): Promise<HttpResponse<string>> {
+    this.validation.validate(request)
+
     await this.createUserImplementation.create(request)
 
     return {
