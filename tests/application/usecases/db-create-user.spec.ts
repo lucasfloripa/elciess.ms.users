eede86b20805c64e9c540fd6e34ecf22a68f27d3
Lucas Gonçalves <lucas.floripa@icloud.com>
2023-01-18 14:36:55 -0300

    feat: ensure DbCreateUserUseCase call checkUserByEmailRepository with correct params

---
+++ tests/application/usecases/db-create-user.spec.ts
@@ -0,0 +1,26 @@
+import { DbCreateUserUseCase } from '../../../src/application/usecases'
+import { CheckUserByEmailRepository } from '../../../src/application/protocols'
+import { mockCreateUserRequest } from '../../domain/mocks'
+import { mockCheckUserByEmailRepositoryStub } from '../mocks'
+
+const mockRequest = mockCreateUserRequest()
+
+interface SutTypes {
+  sut: DbCreateUserUseCase
+  checkUserByEmailRepositoryStub: CheckUserByEmailRepository
+}
+
+const makeSut = (): SutTypes => {
+  const checkUserByEmailRepositoryStub = mockCheckUserByEmailRepositoryStub()
+  const sut = new DbCreateUserUseCase(checkUserByEmailRepositoryStub)
+  return { sut, checkUserByEmailRepositoryStub }
+}
+
+describe('DbCreateUserUseCase', () => {
+  test('Should call checkUserByEmailRepository with correct params', async () => {
+    const { sut, checkUserByEmailRepositoryStub } = makeSut()
+    const spyCreateUserImplementation = jest.spyOn(checkUserByEmailRepositoryStub, 'checkByEmail')
+    await sut.create(mockRequest)
+    expect(spyCreateUserImplementation).toHaveBeenCalledWith(mockRequest.email)
+  })
+})
