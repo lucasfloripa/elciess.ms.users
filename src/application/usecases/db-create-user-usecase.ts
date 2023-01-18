f33b850535014b8a564e5da093f4edbaf4aff087
Lucas Gonçalves <lucas.floripa@icloud.com>
2023-01-18 20:28:18 -0300

    feat: ensure DbCreateUserUseCase calls hasher with correct params

--- src/application/usecases/db-create-user-usecase.ts
+++ src/application/usecases/db-create-user-usecase.ts
@@ -1,18 +1,20 @@
-import { CheckUserByEmailRepository, IdGenerator } from '../protocols'
+import { CheckUserByEmailRepository, IdGenerator, Hasher } from '../protocols'
 import { CreateUserImplementation, CreateUserImplementationParams } from '../../domain/implementation'
 
 export class DbCreateUserUseCase implements CreateUserImplementation {
   constructor (
     private readonly checkUserByEmailRepository: CheckUserByEmailRepository,
-    private readonly idGenerator: IdGenerator
+    private readonly idGenerator: IdGenerator,
+    private readonly hasher: Hasher
   ) {}
 
   async create (params: CreateUserImplementationParams): Promise<boolean> {
-    const { email } = params
+    const { email, password } = params
     const exists = await this.checkUserByEmailRepository.checkByEmail(email)
     const isValid = false
     if (!exists) {
       await this.idGenerator.generate()
+      await this.hasher.hash(password)
     }
     return isValid
   }
