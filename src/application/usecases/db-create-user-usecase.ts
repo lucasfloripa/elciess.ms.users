cab96d12279df0c3a6421beeffc10c9638a0668f
Lucas Gonçalves <lucas.floripa@icloud.com>
2023-01-18 18:14:57 -0300

    feat: ensure DbCreateUserUseCase call idGenerator correctly

--- src/application/usecases/db-create-user-usecase.ts
+++ src/application/usecases/db-create-user-usecase.ts
@@ -1,16 +1,19 @@
-import { CheckUserByEmailRepository } from '../protocols'
+import { CheckUserByEmailRepository, IdGenerator } from '../protocols'
 import { CreateUserImplementation, CreateUserImplementationParams } from '../../domain/implementation'
 
 export class DbCreateUserUseCase implements CreateUserImplementation {
   constructor (
-    private readonly checkUserByEmailRepository: CheckUserByEmailRepository
+    private readonly checkUserByEmailRepository: CheckUserByEmailRepository,
+    private readonly idGenerator: IdGenerator
   ) {}
 
   async create (params: CreateUserImplementationParams): Promise<boolean> {
     const { email } = params
     const exists = await this.checkUserByEmailRepository.checkByEmail(email)
     const isValid = false
-    if (exists) { /* empty */ }
+    if (!exists) {
+      await this.idGenerator.generate()
+    }
     return isValid
   }
 }
