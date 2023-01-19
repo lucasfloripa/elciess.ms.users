c14f223b0d9b2b909e3edb5674994ac942a4af76
Lucas Gonçalves <lucas.floripa@icloud.com>
2023-01-18 21:49:57 -0300

    feat: ensure bcryptAdapter works correctly

---
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -0,0 +1,12 @@
+import { Hasher } from '../../../src/application/protocols'
+import bcrypt from 'bcrypt'
+
+export class BcryptAdapter implements Hasher {
+  constructor (
+    private readonly salt: number
+  ) {}
+
+  async hash (plaintext: string): Promise<string> {
+    return await bcrypt.hash(plaintext, this.salt)
+  }
+}
