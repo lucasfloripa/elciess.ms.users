010a4ad147f31c68d77ff3070f0a043ec408d291
Lucas Gonçalves <lucas.floripa@icloud.com>
2023-01-18 22:00:56 -0300

    feat: ensure uuidAdapter works correctly

---
+++ src/infra/generators/uuid-adapter.ts
@@ -0,0 +1,9 @@
+import { IdGenerator } from '../../../src/application/protocols'
+
+import { v4 as uuidv4 } from 'uuid'
+
+export class UuidAdapter implements IdGenerator {
+  async generate (): Promise<string> {
+    return uuidv4()
+  }
+}
