99366d4d501d4a4bad1b5e997ccb6ac062a92cd2
Lucas Gonçalves <lucas.floripa@icloud.com>
2023-04-30 16:50:21 -0300

    fix: remove unancessary code

--- src/infra/message-brokers/kafka/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './kafka-helper'
-export * from './kafka'
--- src/main/server.ts
+++ src/main/server.ts
@@ -3,11 +3,9 @@ import dotenv from 'dotenv'
 dotenv.config({ path: './.env' })
 
 import { PostgresHelper } from '../infra/db/postgres'
-import { KafkaHelper } from '../infra/message-brokers/kafka'
 
 async function run (): Promise<void> {
   await PostgresHelper.connect()
-  await KafkaHelper.producerConnect()
   const app = (await import('./config/app')).default
   app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${
