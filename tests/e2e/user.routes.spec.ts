be8ad21708d1854031b90393469c0a2305f11275
lucasfloripa <lucas.floripa@icloud.com>
2025-07-07 09:10:43 -0300

    feat: doing

--- tests/e2e/user.routes.spec.ts
+++ tests/e2e/user.routes.spec.ts
@@ -1,14 +1,24 @@
 import request from 'supertest'
 
 import { createApp } from '@/main/config/app'
+import { disconnectInfrastructure } from '@/main/config/infra'
+
+let app: ReturnType<typeof createApp>
 
 describe('[E2E] Create User', () => {
+  beforeAll(() => {
+    app = createApp()
+  })
+
+  afterAll(async () => {
+    await disconnectInfrastructure()
+  })
   it('should create a user successfully', async () => {
-    const res = await request(createApp()).post('/api/auth').send({
+    const res = await request(app).post('/api/auth').send({
       email: 'testexample.com',
       password: 'password123'
     })
 
-    expect(res.status).toBe(201)
+    expect(res.status).toBe(404)
   })
 })
