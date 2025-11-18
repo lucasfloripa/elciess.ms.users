1ecd82dad0a65f8987d5a89b00cd0cf5b4ca6139
lucasfloripa <lucas.floripa@icloud.com>
2025-11-18 14:12:37 -0300

    feat: doing

--- src/domain/errors/invalid-date-format.error.ts
+++
@@ -1,7 +0,0 @@
-export class InvalidDateFormatError extends Error {
-  error: string
-  constructor() {
-    super('Invalid date format. Expected format: yyyy-mm-dd')
-    this.error = 'InvalidDateFormatError'
-  }
-}
